4581e163984b2ac9b64d502570a276de6f8bed62
Michael <mikeh@fnal.gov>
2014-04-06 01:18:47 +0200

    conf changes
    Conflicts:
    	Configuration/StandardSequences/python/Digi_PreMix_cff.py

--- Configuration/StandardSequences/python/Digi_PreMix_cff.py
+++ Configuration/StandardSequences/python/Digi_PreMix_cff.py
@@ -7,10 +7,11 @@ from Configuration.StandardSequences.DigiNZS_cff import *
 
 # modifications to the digi sequences defined above (DigiNZS imports the central Digi_cff)
 
-simMuonRPCDigis.doBkgNoise = False
-simMuonCSCDigis.strips.doNoise = False
-simMuonCSCDigis.wires.doNoise = False
-simMuonDTDigis.onlyMuHits = True
+#simMuonCSCDigis.strips.doNoise = False
+#simMuonCSCDigis.wires.doNoise = False
+#simMuonDTDigis.onlyMuHits = True
+#simMuonRPCDigis.Noise = False
+
 
 # Note: the other noise is turned of in the DigitizersNoNoise sequence defined in the MixingModule
 # because the MM holds/controls all of the other digitizers.
